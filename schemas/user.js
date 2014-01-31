c52e1eac069af1e1fa0d146ebd7f7f7483162b52
joseospina47 <letumactabilis@gmail.com>
2014-01-31 00:36:52 -0500

    App architecture changed

--- schemas/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('User', {
-  email: String,
-  user_id: String,
-  name: String,
-  status: String,
-  third_party_id: String
-});
